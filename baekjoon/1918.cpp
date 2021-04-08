51d2c6905f620a92123b3b72cd77a60014b2f785
greedy0110 <greedy0110@gmail.com>
2021-04-08 17:06:48 +0900

    🚀 slove 1918 (data structure, implementation)
    - 문제를 나눠서 풀 수 있는가?
    - 본인만의 자료구조로 문제를 간단하게 표현할 수 있는가?
    - 결국... 풀 수 있는가?

--- baekjoon/1918.cpp
+++ baekjoon/1918.cpp
@@ -49,7 +49,6 @@ struct Token {
 };
 
 string postfix_notation(const string &str) {
-    cout << str << endl;
     if (str.size() == 1) {
         return str;
     }
@@ -68,7 +67,6 @@ string postfix_notation(const string &str) {
                 if (stack.empty()) {
                     string chunk = str.substr(start + 1, i - start - 1);
                     tokens.push_back(Token{postfix_notation(chunk), 0});
-                    cout << tokens.back().str << endl;
                 }
                 break;
             case '/':
@@ -86,14 +84,60 @@ string postfix_notation(const string &str) {
         }
     }
 
-    string ret = tokens[0].str;
-    for (int i = 1; i < tokens.size(); i += 2) {
-        ret += tokens[i + 1].str + tokens[i].op;
+    int prev_priority = 1;
+    int prev_index = -2;
+    int wrap_index = -2;
+    vector<Token> compressed_tokens;
+    // operation priority 에 따라서 token 을 묶자.
+    for (int i = 0; i < tokens.size(); i++) {
+        char op = tokens[i].op;
+        if (op == 0) continue;
+
+        int priority;
+        if (op == '+' || op == '-') priority = 1;
+        if (op == '*' || op == '/') priority = 2;
+
+        if (priority == 2 && prev_priority == 1) {
+            prev_index = i;
+            wrap_index = -1;
+        } else if (priority == 1 && prev_priority == 2) {
+            wrap_index = i;
+
+            string ret = tokens[prev_index - 1].str;
+            for (int j = prev_index; j < wrap_index; j += 2) {
+                ret += tokens[j + 1].str + tokens[j].op;
+            }
+            compressed_tokens.push_back(Token{ret, 0});
+            compressed_tokens.push_back(tokens[i]);
+        } else if (prev_priority == 1) {
+            compressed_tokens.push_back(tokens[i - 1]);
+            compressed_tokens.push_back(tokens[i]);
+        }
+
+        prev_priority = priority;
+    }
+
+    if (wrap_index == -1) {
+        // prev는 있는데 wrap이 안되었다면? prev_index-1 ~ 끝까지 compress 대상
+        string ret = tokens[prev_index - 1].str;
+        for (int j = prev_index; j < tokens.size(); j += 2) {
+            ret += tokens[j + 1].str + tokens[j].op;
+        }
+        compressed_tokens.push_back(Token{ret, 0});
+    } else {
+        compressed_tokens.push_back(tokens.back());
+    }
+
+    string ret = compressed_tokens[0].str;
+    for (int i = 1; i < compressed_tokens.size(); i += 2) {
+        ret += compressed_tokens[i + 1].str + compressed_tokens[i].op;
     }
 
     return ret;
 }
 
 void solve() {
-    cout << postfix_notation("A*(B+C)") << endl;
+    string str;
+    cin >> str;
+    cout << postfix_notation(str) << endl;
 }
